afe565caf77c4eb543c1dc18830f6af9cb8d033c
Programacion 2 <programacion2@interaktu.com>
2023-10-04 12:10:40 +0200

    Refactorize and restyle the aplicaction

--- DT_Money/JS/form.js
+++ DT_Money/JS/form.js
@@ -1,41 +1,57 @@
-const transactions = [];
+let transactions = [];
 const INITIAL_VALUES = {
   totalEntrys: 0,
   totalExits: 0,
   totalBalance: 0,
 };
+const jsonData = localStorage.getItem("transactions");
+const table = document.querySelector("#info table");
+
+// function recalculTotals() {
+
+//   const totals = transactions.reduce((totals,transaction) => {
+
+//     if (transaction.flag === "1") {
+//       totals.totalEntrys += transaction.price;
+//     } else if (transaction.flag === "0") {
+//       totals.totalExits += transaction.price;
+//     }
+
+//     return totals;
+//   }, INITIAL_VALUES);
+//   console.log(INITIAL_VALUES)
+// }
 
 function printLine(transaction) {
-  //search table
-  const table = document.querySelector("#info table");
-  //create a tr
   const newRow = document.createElement("tr");
   //Create ftitle as a td
   const titleCell = document.createElement("td");
   titleCell.textContent = transaction.title;
-  titleCell.className = "normal-text";
-   //Create fprice as a td and assign class(if transaction.flag(radiobutton value) it's entry or exit) and format the price to €
+  titleCell.className = "title-text";
+  //Create fprice as a td and assign class(if transaction.flag(radiobutton value) it's entry or exit) and format the price to €
   const priceCell = document.createElement("td");
-  if (transaction.flag == '1') {
+  if (transaction.flag == "1") {
     priceCell.className = "green-text";
-    priceCell.textContent =  new Intl.NumberFormat("de-DE", {
+    priceCell.textContent = new Intl.NumberFormat("de-DE", {
       style: "currency",
       currency: "EUR",
     }).format(transaction.price);
-  } else if (transaction.flag == '0') {
+  } else if (transaction.flag == "0") {
     priceCell.className = "red-text";
-    priceCell.textContent = '- '+ new Intl.NumberFormat("de-DE", {
-      style: "currency",
-      currency: "EUR",
-    }).format(transaction.price);
+    priceCell.textContent =
+      "- " +
+      new Intl.NumberFormat("de-DE", {
+        style: "currency",
+        currency: "EUR",
+      }).format(transaction.price);
   }
   //Create the category as a td
   const categoryCell = document.createElement("td");
   categoryCell.textContent = transaction.category;
   categoryCell.className = "normal-text";
-  //Create the date as a td 
+  //Create the date as a td
   const dateCell = document.createElement("td");
-  dateCell.textContent = transaction.date.toLocaleDateString();;
+  dateCell.textContent = transaction.date;
   dateCell.className = "normal-text";
   //append the td's to the tr
   newRow.appendChild(titleCell);
@@ -47,47 +63,75 @@ function printLine(transaction) {
 }
 
 function recalculTotals(transaction) {
+  if (transaction.flag === "1") {
+    INITIAL_VALUES.totalEntrys += transaction.price;
+  } else if (transaction.flag === "0") {
+    INITIAL_VALUES.totalExits += transaction.price;
+  }
+  INITIAL_VALUES.totalBalance =
+    INITIAL_VALUES.totalEntrys - INITIAL_VALUES.totalExits;
+  console.log(INITIAL_VALUES);
 
-  const totals = transactions.reduce((totals,transaction) => {
-    
-    console.log(totals)
-    console.log(transaction)
-    if (transaction.flag === "1") {
-      totals.totalEntrys += transaction.price;
-    } else if (transaction.flag === "0") {
-      totals.totalExits += transaction.price;
-    }
-    
-
-    return totals;
-  }, INITIAL_VALUES);
-  console.log(INITIAL_VALUES)
+  return INITIAL_VALUES;
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.querySelector("form");
-  let totalEntrys = new Decimal(0);
-  let totalExits = new Decimal(0);
-  let totalBalance = new Decimal(0);
-
-  // Update the UI with the loaded values
+function printBalance() {
   const entrynSpan = document.getElementById("entryn");
   entrynSpan.textContent = new Intl.NumberFormat("de-DE", {
     style: "currency",
     currency: "EUR",
-  }).format(totalEntrys);
+  }).format(INITIAL_VALUES.totalEntrys);
 
   const exitnSpan = document.getElementById("exitn");
   exitnSpan.textContent = new Intl.NumberFormat("de-DE", {
     style: "currency",
     currency: "EUR",
-  }).format(totalExits);
+  }).format(INITIAL_VALUES.totalExits);
 
   const balancenSpan = document.getElementById("balancen");
   balancenSpan.textContent = new Intl.NumberFormat("de-DE", {
     style: "currency",
     currency: "EUR",
-  }).format(totalBalance);
+  }).format(INITIAL_VALUES.totalBalance);
+  const balancenDiv = document.getElementById("balance");
+  if (INITIAL_VALUES.totalBalance >= 0) {
+    balancenDiv.className = "green-balance";
+  } else {
+    balancenDiv.className = "red-balance";
+  }
+}
+
+function loadLocalStorage() {
+  transactions = JSON.parse(jsonData);
+  
+  transactions.forEach((transaction) => {
+    const transactionstoPrint = {
+      title: transaction.title,
+      price: parseFloat(transaction.price),
+      category: transaction.category,
+      date: transaction.date,
+      flag: transaction.flag,
+    };
+    const jsonData = JSON.stringify(transactions);
+    console.log(transactionstoPrint);
+    printLine(transactionstoPrint);
+    recalculTotals(transactionstoPrint);
+    printBalance();
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("form");
+
+  if (jsonData) {
+    console.log("Cargando datos desde el local storage");
+    loadLocalStorage();
+  } else {
+    console.log(
+      "No se puede cargar desde local storage ya que no hay ningun registro guardado"
+    );
+    printBalance();
+  }
 
   form.addEventListener("submit", function (e) {
     e.preventDefault();
@@ -99,19 +143,22 @@ document.addEventListener("DOMContentLoaded", function () {
       title: data.fname,
       price: parseFloat(data.fprice),
       category: data.fcategory,
-      date: new Date(),
+      date: new Date().toLocaleDateString(),
       flag: data.type,
     };
 
-    console.log(transactionstoSave);
+    console.log("transaction" + transactionstoSave);
     transactions.push(transactionstoSave);
+    const jsonData = JSON.stringify(transactions);
+
+    // Guarda la string en local storage
+    localStorage.setItem("transactions", jsonData);
+
+    console.log("transaction saved" + jsonData);
     form.reset();
 
     printLine(transactionstoSave);
     recalculTotals(transactionstoSave);
-
-
-    const jsonData = JSON.stringify(transactions);
-
+    printBalance();
   });
 });
